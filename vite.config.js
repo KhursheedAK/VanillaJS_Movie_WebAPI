bda84a633fafcf8228d02af5af3fc1f813236dbf
Khursheed <ka.pixelbits@gmail.com>
2025-12-04 01:05:57 +0500

    fixed css paths

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/https://github.com/KhursheedAK/VanillaJS_Movie_WebAPI.git', // <-- your repo name
+  base: '/VanillaJS_Movie_WebAPI',
 });
